b8ce64b5e851acad543ab8913dca4eeae1647f0e
David Hristov <orionvt3@gmail.com>
2023-12-23 22:02:25 +0200

    TwoSum O(n2) solution

---
+++ 1.TwoSum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _1.TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = TwoSum(new[] { 3, 2 ,4}, 6);
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int pair = target - nums[i];
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    if (nums[j] == pair)
+                    {
+                        return new int[] {i, j};
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
