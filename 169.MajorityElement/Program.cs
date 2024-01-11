8500d0943213f98e2d48890a0a6e560e87a47a81
David Hristov <orionvt3@gmail.com>
2024-01-11 20:18:10 +0200

    169 Majority Element Beats 76

---
+++ 169.MajorityElement/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _169.MajorityElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MajorityElement(new int[] { 2, 2, 1, 1, 1, 2, 2 }));
+        }
+        public static int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+            
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!counts.ContainsKey(nums[i]))
+                {
+                    counts.Add(nums[i], 1);
+                }
+                else
+                {
+                    counts[nums[i]]++;
+                }
+            }
+
+            var maxVal = counts.Values.Max();
+            return counts.First(k => k.Value == maxVal).Key;
+        }
+    }
+}
