a8a71306b310b64be9e731a8a02d7699d69e9d2f
David Hristov <orionvt3@gmail.com>
2024-02-05 22:11:54 +0200

    387 daily challenge

---
+++ 387.FirstUniqueCharacterInString/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _387.FirstUniqueCharacterInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public static int FirstUniqChar(string s)
+        {
+            var dict = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dict.ContainsKey(s[i]))
+                {
+                    dict[s[i]]++;
+                }
+                else
+                {
+                    dict.Add(s[i], 1);
+                }
+            }
+            var sym = dict.FirstOrDefault(x => x.Value == 1);
+            if (sym.Value == 0)
+            {
+                return -1;
+            }
+
+            return s.IndexOf(sym.Key);
+        }
+    }
+}
